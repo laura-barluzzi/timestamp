378c45afdb123e11e78bf44ed50ad2539fa594bb
Laura Barluzzi <laura@koalacoder.com>
2017-03-27 23:18:39 +0000

    take unix time and show result

--- server.js
+++ server.js
@@ -1,12 +1,47 @@
 var express = require('express');
 var app = express();
 
+
+function isValidUnix(sec){
+  var milSeconds = (sec * 1000);
+  var date = new Date(milSeconds);
+  return (date != 'Invalid Date') ? true : false; 
+}
+
+function getDate(ms) {
+  var months = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"];
+  var date = new Date(ms);
+  var naturalDate = months[date.getMonth()] + ", " +
+                    date.getDate() + " " + date.getFullYear();
+  return naturalDate;
+}
+
+function isNaturalDate(param) {
+  return false;
+}
+
+function getResult(unix, natural) {
+  return {"unix": unix,
+          "natural": natural};
+}
+
 app.get('/:date', function (req, res) {
   var paramDate = req.params.date; //string
+  var isNaturalDate = true;
+  var result = "";
   
-  var result = {  "unix": null,
-                  "natural": null  }
-               
+  if (isValidUnix(paramDate)) {
+    var ms = parseInt(paramDate*1000);
+    var naturalDate = getDate(ms);
+    result = getResult(ms, naturalDate);
+
+  } else if (isNaturalDate) {
+    //from date to unix
+  } else {
+    result = getResult(null, null);
+  }
+  
   res.send(JSON.stringify(result));
 });
 
